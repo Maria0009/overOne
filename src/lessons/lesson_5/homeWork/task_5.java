7b269d4cb86e41d4c9b9e56e07b33954ed25ed9d
Maria0009 <bashkova.mari@bk.ru>
2022-01-22 20:22:15 +0300

    third commit

--- src/lessons/lesson_5/homeWork/task_5.java
+++ src/lessons/lesson_5/homeWork/task_5.java
@@ -16,7 +16,79 @@ public class task_5 {
             }
             System.out.println();
         }
+        for (int i = 0; i < 4; i++) {
+            switch (i) {
+                case 0:
+                    System.out.print("         ");
+                    break;
+                case 1:
+                    System.out.print("      ");
+                    break;
+                case 2:
+                    System.out.print("   ");
+                    break;
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 3; j >= i; j--) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+            switch (i) {
+                case 0:
+                    System.out.print("   ");
+                    break;
+                case 1:
+                    System.out.print("      ");
+                    break;
+                case 2:
+                    System.out.print("         ");
+                    break;
+            }
+        }
 
+//rhombus
+        System.out.println();
+        for (int i = 1; i < 8; i += 2) {
+            switch (i) {
+                case 1:
+                    System.out.print("         ");
+                    break;
+                case 3:
+                    System.out.print("      ");
+                    break;
+                case 5:
+                    System.out.print("   ");
+                    break;
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 8; i += 2) {
+            for (int j = 7; j >= i; j--) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+            switch (i) {
+                case 0:
+                    System.out.print("   ");
+                    break;
+                case 2:
+                    System.out.print("      ");
+                    break;
+                case 4:
+                    System.out.print("         ");
+                    break;
+            }
+        }
+        System.out.println();
 
 
     }
