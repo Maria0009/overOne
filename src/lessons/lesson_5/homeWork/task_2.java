f7bc99f96286978b99329d02d3d17a3661a07fc4
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-22 21:11:05 +0300

    Update task_2.java

--- src/lessons/lesson_5/homeWork/task_2.java
+++ src/lessons/lesson_5/homeWork/task_2.java
@@ -2,11 +2,12 @@
 package lessons.lesson_5.homeWork;
 
 import java.util.Scanner;
-
+//+'
 public class task_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 2 number:");
+        //naming, please
         double A = scanner.nextDouble();
         double B = scanner.nextDouble();
         double multiplication = 0;
