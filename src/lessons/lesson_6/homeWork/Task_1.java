873c4f22b9a40e567946b62c1e83e368dea8db2f
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-23 21:13:28 +0300

    Update Task_1.java

--- src/lessons/lesson_6/homeWork/Task_1.java
+++ src/lessons/lesson_6/homeWork/Task_1.java
@@ -1,6 +1,6 @@
 //Вычислить: 1+2+4+8+...+256
 package lessons.lesson_6.homeWork;
-
+//+
 public class Task_1 {
     public static void main(String[] args) {
         final int LIMIT = 256;
@@ -8,6 +8,7 @@ public class Task_1 {
         int i = 1;
         while (i <= LIMIT) {
             sum += i;
+            //do not left commented code
             //System.out.println(i+" "+sum);
             i *= 2;
         }
