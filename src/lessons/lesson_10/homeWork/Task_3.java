baa0d6406a5cd749f5d2d4f3430b395e1dfc921a
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-02-07 21:38:41 +0300

    Update Task_3.java

--- src/lessons/lesson_10/homeWork/Task_3.java
+++ src/lessons/lesson_10/homeWork/Task_3.java
@@ -2,7 +2,7 @@
 package lessons.lesson_10.homeWork;
 
 import java.util.Scanner;
-
+//+
 public class Task_3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
