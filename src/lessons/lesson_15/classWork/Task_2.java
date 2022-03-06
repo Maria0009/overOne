f6b173142be6453cf2bd8d01b650ecb0b5eb3048
Maria0009 <bashkova.mari@bk.ru>
2022-03-06 11:45:41 +0300

    task2

---
+++ src/lessons/lesson_15/classWork/Task_2.java
@@ -0,0 +1,19 @@
+package lessons.lesson_15.classWork;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] str = initString(scanner).split(" \\w");
+        System.out.println(Arrays.toString(str));
+        int counterWord = str.length;
+        System.out.println(counterWord);
+    }
+
+    static String initString(Scanner scanner) {
+        return scanner.nextLine();
+    }
+
+}
