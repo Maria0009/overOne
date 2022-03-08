bc45034151568395332d579bc901432c256d97f9
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-03-08 15:04:44 +0300

    Update Task_8.java

--- src/lessons/lesson_15/homeWork/Task_8.java
+++ src/lessons/lesson_15/homeWork/Task_8.java
@@ -1,6 +1,7 @@
 package lessons.lesson_15.homeWork;
 
 //Есть строка “Green. red. blue. yellow”. Необходимо заменить все точки на запятые
+//+
 public class Task_8 {
     public static void main(String[] args) {
         String str = "Green. red. blue. yellow";
