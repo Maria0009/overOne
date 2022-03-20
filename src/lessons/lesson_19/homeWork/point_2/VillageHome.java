e2fc49883c8574e3abd4ac5e1291b26b6057b559
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-03-20 22:04:12 +0300

    Update VillageHome.java

--- src/lessons/lesson_19/homeWork/point_2/VillageHome.java
+++ src/lessons/lesson_19/homeWork/point_2/VillageHome.java
@@ -1,10 +1,10 @@
 package lessons.lesson_19.homeWork.point_2;
-
+//+
 public class VillageHome extends Home {
 
     public VillageHome(int numberOfFloors, String heating, int numberOfPeople) {
         super(numberOfFloors, heating, numberOfPeople);
     }
-
-
+//delete
+//delete
 }
