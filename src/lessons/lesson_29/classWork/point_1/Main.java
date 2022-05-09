9e3b0f75b9d01b44f0201025d875e84a56de53b9
Maria0009 <bashkova.mari@bk.ru>
2022-05-09 23:01:20 +0300

    Task

---
+++ src/lessons/lesson_29/classWork/point_1/Main.java
@@ -0,0 +1,15 @@
+package lessons.lesson_29.classWork.point_1;
+
+public class Main {
+    public static void main(String[] args) {
+        //Потоки (с большой буква)
+        //New => start => Runable => run => Running => stop => Dead
+        //меленькими это методы
+        //Blocked => notify => Runable
+
+        Person person1 = new Person("Ivan", "Max", "Vasiliy");
+        Person person2 = new Person("Sergey", " Alex", "Roman");
+        person1.start();
+        person2.start();
+    }
+}
---
+++ src/lessons/lesson_29/classWork/point_1/Person.java
@@ -0,0 +1,21 @@
+package lessons.lesson_29.classWork.point_1;
+// implements  Runnable // лучше использовать implements
+public class Person extends  Thread  {
+
+    String[] strs;
+    public  Person(String...strs){
+        this.strs= strs;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < strs.length ; i++) {
+            System.out.println("Hello "+ strs[i]);
+            try {
+                sleep(500);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
